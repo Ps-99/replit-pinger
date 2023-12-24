11ba82fc8a5a02e89322fdf9bd41671ac5c83fbf
Dornox <69315835+krushna06@users.noreply.github.com>
2023-12-24 14:50:44 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,53 @@
+# Replit-Pinger
+
+Keep your Replit/any other projects alive with this simple pinger.
+
+# Usage
+
+If you want to add your own urls to the pinger :-
+1. Fork this repo.
+2. Edit the urls array in index.js to add your own urls.
+3. Push the changes to your forked repo.
+4. Go to https://github.com/krushna06/replit-pinger and click on the "New Pull Request" button.
+5. Open a pull request and wait for it to be merged.
+6. Once the pull request is merged, your urls will be added to the pinger.
+
+## Installation
+
+1. Make sure you have Node.js and npm installed on your system.
+2. Clone this repository or download the code.
+3. Open a terminal and navigate to the project directory.
+4. Run the following command to install the dependencies:
+
+```bash
+npm install
+```
+
+# Configuration
+
+1. Open the index.js file in a text editor.
+2. Edit the urls array to specify the URLs you want to fetch data from.
+3. Save the file.
+
+# Deployment
+1. Start the server by running the following command:
+
+```bash
+npm start
+```
+
+2. The server will start running on port 3000.
+3. Open your web browser and navigate to http://localhost:3000 to confirm that the server is running.
+4. Use the following endpoints:
+
+-   /status: Returns the status of each URL.
+-   /keepalive: Confirms that the keep-alive functionality is working.
+
+# Customization
+
+-   You can modify the fetch interval in the setInterval call in the keepReplAlive function.
+-   Customize the URLs in the urls array to suit your requirements.
+
+# License
+
+This project is licensed under the MIT License.
---
+++ index.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const axios = require("axios");
+
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+
+const urlClusters = [
+  {
+    name: "Cluster 1",
+    urls: [
+      "https://discord.krushnavyas.repl.co/",
+    ],
+  },
+  // Add more URL clusters with names here
+];
+
+let statusList = {};
+
+const fetchData = async (url) => {
+  try {
+    const response = await axios.get(url, { timeout: 5000 }); // 5 seconds timeout
+    const status = response.status;
+    return status;
+  } catch (error) {
+    console.error(`Error fetching data from ${url}:`, error.message);
+    return error.message;
+  }
+};
+
+const keepReplAlive = async () => {
+  // Fetch data for each URL cluster in parallel
+  statusList = {};
+
+  // Fetch data for each URL cluster in parallel
+  await Promise.all(
+    urlClusters.map(async (cluster) => {
+      const clusterName = cluster.name;
+      const clusterStatus = {};
+      const promises = cluster.urls.map(async (url) => {
+        try {
+          const status = await fetchData(url);
+          clusterStatus[url] = status;
+        } catch (error) {
+          console.error(`Error fetching data from ${url}:`, error.message);
+          clusterStatus[url] = "Error"; // Handle the error case
+        }
+      });
+      await Promise.race([Promise.all(promises), new Promise(resolve => setTimeout(resolve, 25000))]);
+      statusList[clusterName] = clusterStatus;
+    })
+  );
+};
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+app.get("/status", async (req, res) => {
+  await keepReplAlive(); // Fetch the latest status before responding to /status
+  console.log(statusList);
+  res.json(statusList);
+});
+
+app.get("/keepalive", (req, res) => {
+  res.send("Keep-alive is working!");
+});
+
+module.exports = app;
